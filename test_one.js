c2c0e1d46439e3a73b9687e3bd9f76815b68ce28
kusal98 <kusalrokes@gmail.com>
2021-06-21 11:25:33 +0530

    Added interval and set timeout

---
+++ test_one.js
@@ -0,0 +1,27 @@
+//const name ='mari';
+//console.log(name);
+
+
+//make greatings
+// const greet = (name) => {
+//     console.log(`hello, ${name}`);
+// }
+// greet('kusal');
+// greet('aliya');
+
+
+// //tomeout and intervals
+// console.log(global);
+// global.setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//     console.log('im the interval');
+// }, 1000)
+
+// console.log(__dirname);
+// console.log(__filename)
+
+console.log(document.querySelector);
